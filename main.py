95960a6cf4bb30e60d1fbbdf392e0c6ffe2578b5
Przemyslaw Kot <przemekkot@wp.pl>
2020-07-12 17:38:14 +0200

    Develop project, new files, new functionalities

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for, request
 import tmdb_client
+import random
+from random import choice
+from markupsafe import escape
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list)
 
 
 @app.context_processor
@@ -16,3 +21,10 @@ def utility_processor():
     return {"tmdb_image_url": tmdb_image_url}
 
 
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+   details = tmdb_client.get_single_movie(movie_id)
+   cast = tmdb_client.get_single_movie_cast(movie_id, number=10)
+   movie_images = tmdb_client.get_movie_images(movie_id)
+   selected_backdrop = random.choice(movie_images['backdrops'])
+   return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop, movie_images=movie_images)
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,5 +1,9 @@
 import json
 import requests
+import random
+from random import shuffle
+
+available_lists = ['latest', 'top_rated', 'upcoming', 'popular', 'now_playing']
 
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
@@ -12,12 +16,58 @@ def get_popular_movies():
 
 #print(get_popular_movies())
 
-def get_poster_url(poster_api_path, size="w780"):
+def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
 
 #print(get_poster_url("/db32LaOibwEliAmSL2jjDF6oDdj.jpg"))
 
-def get_movies(how_many):
-    data = get_popular_movies()
-    return data["results"][:how_many]
+def get_movies(how_many, list_type):
+    #data = get_popular_movies()
+    data = get_movies_list(list_type)
+    movies = data["results"]
+    random.shuffle(movies)
+    return movies[:how_many]
+
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTE4Y2NkODY1MWUyMDczOWZhYTRlMzU4Y2I2ZDIwZCIsInN1YiI6IjVmMDU2OGQwMjBhZjc3MDAzNWU3N2IzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VnK58vvU8DOSdgzhlxiHt-PfHTwIVpsXelEW4k47kgA"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_single_movie_cast(movie_id, number):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTE4Y2NkODY1MWUyMDczOWZhYTRlMzU4Y2I2ZDIwZCIsInN1YiI6IjVmMDU2OGQwMjBhZjc3MDAzNWU3N2IzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VnK58vvU8DOSdgzhlxiHt-PfHTwIVpsXelEW4k47kgA"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"][:number]
+
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTE4Y2NkODY1MWUyMDczOWZhYTRlMzU4Y2I2ZDIwZCIsInN1YiI6IjVmMDU2OGQwMjBhZjc3MDAzNWU3N2IzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VnK58vvU8DOSdgzhlxiHt-PfHTwIVpsXelEW4k47kgA"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTE4Y2NkODY1MWUyMDczOWZhYTRlMzU4Y2I2ZDIwZCIsInN1YiI6IjVmMDU2OGQwMjBhZjc3MDAzNWU3N2IzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VnK58vvU8DOSdgzhlxiHt-PfHTwIVpsXelEW4k47kgA"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
